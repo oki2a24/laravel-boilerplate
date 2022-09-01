478ff5a25196d2ca17ff25f92a1a825658c1fddb
oki2a24 <oki2a24@gmail.com>
2022-09-01 17:36:16 +0900

    chore: Chrome DevTools で発生していたエラーを多分解消する
    ```
    client.ts:48 [vite] failed to connect to websocket.
    your current setup:
      (browser) localhost:5173/ <--[HTTP]--> localhost:5173/ (server)
      (browser) localhost:5173/ <--[WebSocket (failing)]--> localhost:5173/ (server)
    Check out your Vite / network configuration and https://vitejs.dev/config/server-options.html#server-hmr .
    ```

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -29,8 +29,10 @@ export default defineConfig({
     ],
     server: {
         host: true,
+        port: "5173",
         hmr: {
             host: 'localhost',
+            port: "5173",
         },
     },
 });
