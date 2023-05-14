603b1b08d3c1b2bccdf7aa9fc518204fa342e1af
oki2a24 <oki2a24@gmail.com>
2023-05-14 16:14:13 +0900

    feat: フロントの Swiper の試行錯誤を削除する

--- laravel/resources/js/views/MyHome.vue
+++ laravel/resources/js/views/MyHome.vue
@@ -1,8 +1,3 @@
 <template>
   <h1>Home</h1>
-  <ul>
-    <li><router-link :to="{ name: 'Swiper1' }">Swiper の例 1</router-link></li>
-    <li><router-link :to="{ name: 'Swiper2' }">Swiper の例 2</router-link></li>
-    <li><router-link :to="{ name: 'Swiper3' }">Swiper の例 3</router-link></li>
-  </ul>
 </template>
