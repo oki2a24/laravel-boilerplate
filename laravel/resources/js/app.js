b37c53c504c79b1cf6c34896e526a250b32a2558
oki2a24 <oki2a24@gmail.com>
2023-11-11 15:41:37 +0900

    chore: Bootstrap5 を導入する

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -3,5 +3,7 @@ import "./bootstrap";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createApp(App).use(router).mount("#app");
