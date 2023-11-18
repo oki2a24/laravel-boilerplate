b391258dcd8522ac299e08d3cb692b4b7a6a4f1b
oki2a24 <oki2a24@gmail.com>
2023-11-18 10:03:54 +0900

    chore: rector laravel のルールを設定する

--- laravel/rector.php
+++ laravel/rector.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
+use RectorLaravel\Set\LaravelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/app',
-        __DIR__ . '/bootstrap',
         __DIR__ . '/config',
+        __DIR__ . '/database',
         __DIR__ . '/lang',
-        __DIR__ . '/node_modules',
         __DIR__ . '/public',
         __DIR__ . '/resources',
         __DIR__ . '/routes',
@@ -23,7 +23,8 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
     // define sets of rules
-    //    $rectorConfig->sets([
-    //        LevelSetList::UP_TO_PHP_80
-    //    ]);
+    $rectorConfig->sets([
+        // LevelSetList::UP_TO_PHP_80
+        LaravelSetList::LARAVEL_90,
+    ]);
 };
