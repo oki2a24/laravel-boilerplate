e2398bf492868168aa4c0af041be435bcd058d2a
oki2a24 <oki2a24@gmail.com>
2023-11-24 17:45:43 +0900

    feat: `POST api/login` を laravel/breeze を参考に実装する

--- laravel/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ laravel/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,8 +4,32 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthenticatedSessionController extends Controller
 {
-    //
+    /**
+     * Handle an incoming authentication request.
+     *
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): Response
+    {
+        // TODO リクエストフォームクラスに分離する
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (! Auth::attempt($request->only('email', 'password'))) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->noContent();
+    }
 }
--- laravel/tests/Feature/Http/Controllers/Auth/AuthenticatedSessionControllerTest.php
+++ laravel/tests/Feature/Http/Controllers/Auth/AuthenticatedSessionControllerTest.php
@@ -2,21 +2,43 @@
 
 namespace Tests\Feature\Http\Controllers\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AuthenticatedSessionControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function 正しい情報で認証できること(): void
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt($password = 'i-love-laravel'),
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        $response->assertStatus(204);
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function 不正な情報では認証できないこと(): void
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt('i-love-laravel'),
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => $user->email,
+            'password' => 'wrong-password',
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertGuest();
     }
 }
