ff15a5b3eabbbbbfbe3d6feb38e2e27a18f09713
oki2a24 <oki2a24@gmail.com>
2024-01-25 16:22:23 +0900

    feat: laravel/breeze, laravel/fortify を元にメール確認 `POST api/email/verify/{id}/{hash}` を実装する
    実装の参考
    - メール確認 10.x Laravel https://readouble.com/laravel/10.x/ja/verification.html
    - URL
        - https://github.com/laravel/breeze/blob/1.x/stubs/api/routes/auth.php#L27-L33
            - こちらの URL は採用しない。
        - https://github.com/laravel/fortify/blob/1.x/routes/routes.php#L88-L94
            - こちらを採用する。
    - 実装
        - https://github.com/laravel/breeze/blob/1.x/stubs/api/app/Http/Controllers/Auth/VerifyEmailController.php#L16
        - https://github.com/laravel/fortify/blob/1.x/src/Http/Controllers/VerifyEmailController.php#L18
    - テスト
        - https://github.com/laravel/breeze/blob/1.x/stubs/api/tests/Feature/Auth/EmailVerificationTest.php
        - https://github.com/laravel/fortify/blob/1.x/tests/VerifyEmailControllerTest.php

---
+++ laravel/app/Http/Controllers/Auth/VerifyEmailController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\JsonResponse;
+
+class VerifyEmailController extends Controller
+{
+    /**
+     * 認証されたユーザーのメールアドレスを検証済みとしてマークします。
+     */
+    public function __invoke(EmailVerificationRequest $request): JsonResponse
+    {
+        /** @var User $user */
+        $user = $request->user();
+
+        if ($user->hasVerifiedEmail()) {
+            return response()->json('', 204);
+        }
+
+        $request->fulfill();
+
+        return response()->json('', 204);
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\HealthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,7 @@ Route::get('/health', [HealthController::class, 'health'])->name('health');
 
 Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest')->name('register');
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware('guest')->name('login');
+Route::get('/email/verify/{id}/{hash}', VerifyEmailController::class)->middleware(['auth', 'signed', 'throttle:6,1'])->name('verification.verify');
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('logout');
 
