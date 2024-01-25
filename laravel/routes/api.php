9596be5ed824fbebb997d0e300364903e6132b87
oki2a24 <oki2a24@gmail.com>
2024-01-25 16:28:39 +0900

    doc: api を分類する

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -21,11 +21,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/health', [HealthController::class, 'health'])->name('health');
 
-Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest')->name('register');
+// 認証
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware('guest')->name('login');
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('logout');
+// 登録
+Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest')->name('register');
+// メールアドレス確認
 Route::get('/email/verify/{id}/{hash}', VerifyEmailController::class)->middleware(['auth', 'signed', 'throttle:6,1'])->name('verification.verify');
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('logout');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
