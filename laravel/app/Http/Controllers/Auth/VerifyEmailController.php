e9e0765a7b23280918d815aff16f0648eb78b350
oki2a24 <oki2a24@gmail.com>
2024-02-03 17:18:39 +0900

    fixup! fix: 細かい修正をする

--- laravel/app/Http/Controllers/Auth/VerifyEmailController.php
+++ laravel/app/Http/Controllers/Auth/VerifyEmailController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\User;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\JsonResponse;
 
