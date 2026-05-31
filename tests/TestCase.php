0c30a6ee8ae7b542eaabd96b9d33983193d4ee59
oki2a24 <oki2a24@gmail.com>
2026-05-31 17:58:02 +0900

    Add Vite manifest stubbing to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,31 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected ?string $createdManifest = null;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $manifestPath = public_path('build/manifest.json');
+        if (!file_exists($manifestPath)) {
+            @mkdir(public_path('build'), 0777, true);
+            file_put_contents($manifestPath, json_encode(['resources/js/app.js' => ['file' => 'js/app.js', 'src' => 'resources/js/app.js', 'isEntry' => true]]));
+            $this->createdManifest = $manifestPath;
+        }
+    }
+
+    protected function tearDown(): void
+    {
+        if (isset($this->createdManifest) && file_exists($this->createdManifest)) {
+            @unlink($this->createdManifest);
+            $buildDir = dirname($this->createdManifest);
+            if (is_dir($buildDir)) {
+                @rmdir($buildDir);
+            }
+        }
+
+        parent::tearDown();
+    }
 }
